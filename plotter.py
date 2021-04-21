c2e51657d1068dcfee5917a3604ae3895cc3a3d0
Sergio Solano <sergio.solano.p@gmail.com>
2021-04-21 14:23:40 -0500

    merged all the analysis: date, strings, and numerical. Added also the ploting to cli.

--- plotter.py
+++ plotter.py
@@ -3,6 +3,17 @@ class Plotter:
     def __init__(self, max_width):
         self.max_width = max_width
 
+        self.color_palette = (
+            '\x1b[7;30;40m',
+            '\x1b[7;30;41m',
+            '\x1b[7;30;42m',
+            '\x1b[7;30;43m',
+            '\x1b[7;30;44m',
+            '\x1b[7;30;45m',
+            '\x1b[7;30;46m',
+            '\x1b[7;30;47m',
+            )
+
     def set_values(self, xs, ys, x_title, y_title):
         if len(xs) != len(ys):
             raise ValueError('xs and ys should have the same lengths.')
@@ -26,27 +37,29 @@ class Plotter:
         else:
             print('╭' + '─' * (self.max_width - 2) + '╮')
 
-        # titles
-        title_str = '│ ' + self.x_title + ' ' + self.y_title
-        print(f'{title_str}', ' ' * (self.max_width - len(title_str) - 2) + '│')
-
         # normalization to fit in the max_width space
         max_y = max(self.ys)
         max_y_str = len(str(max_y))
-        y_bar_space = self.max_width - 11 - max_y_str - 3
+        y_bar_space = self.max_width - 11 - max_y_str - 8
         xs_width = max(len(self.x_title), len(str(max(self.xs))))
 
+        # titles
+        title_str = '│ ' + f'{self.x_title:{xs_width}}' + ' ' + self.y_title
+        print(f'{title_str}', ' ' * (self.max_width - len(title_str) - 2) + '│')
 
-        CRED = '\033[91m'
-        CEND = '\033[0m'
+        # color end
+        CEND = '\x1b[0m'
+
+        color_index = 0
 
         for x, y in zip(self.xs, self.ys):
             y_bar = y * y_bar_space // max_y
-            # data_string = '│ {0:>{4}} [{1:{2}}] {3}'.format(x, y, max_y_str, '█' * y_bar, xs_width)
-            # print(f'{data_string}', ' ' * (self.max_width - len(data_string) - 3), '│')
-
-            data_string = CRED + '│ {0:>{4}} [{1:{2}}] {3}'.format(x, y, max_y_str, '█' * y_bar, xs_width) + CEND
-            print(f'{data_string}', ' ' * (self.max_width - len(data_string) - 3), '│')
+            
+            data_string = '│ {0:>{3}} [{1:{2}}]'.format(str(x), y, max_y_str, xs_width)
+            bars_string = self.color_palette[color_index] + '{0}'.format('█' * y_bar) + CEND
+            print(data_string, bars_string, ' ' * (self.max_width - len(data_string) - len(bars_string) + 10), '│')
+            color_index = (color_index  + 1) % len(self.color_palette)
+            # print(x, y, xs_width)
 
         print('╰' + '─' * (self.max_width - 2) + '╯')
 
--- rda_v2.py
+++ rda_v2.py
@@ -28,21 +28,23 @@ def load_data(file_prefix = 'rdat_metro'):
             columns.append(df_tmp.columns)
             df = df.append(df_tmp)
             
-    #print(df.shape)
     return df
 
 
 
 ##############################
-# HOURLY REPORTS
+# TIME-RELATED REPORTS
 ##############################
-def hourly_reports(df, cm_to_filter = None):
+def hourly_reports(df, plot_length, col = '', cm_to_filter = None):
     cols = [
         'HORATLMI',
         'HORACTEI',
         'HORATLMF',
         'HORA_REAL',
     ]
+
+    if col in cols:
+        cols = [col]
 
     if cm_to_filter is not None:
         df = df[df['CMANTENI'] == cm_to_filter]
@@ -63,10 +65,10 @@ def hourly_reports(df, cm_to_filter = None):
             except KeyError:
                 ys.append(0)
                 
-        plot = plt(80)
+        plot_length = plot_length if plot_length < 143 else 142
+        plot = plt(plot_length)
         plot.set_values(xs, ys, 'HORA', 'Reportes')
         plot.show(col)
-
 
         # Análisis por turno
         xys = {
@@ -83,25 +85,96 @@ def hourly_reports(df, cm_to_filter = None):
             else:
                 xys['noct'] += y
 
-        plot = plt(80)
+
+        plot = plt(plot_length)
         plot.set_values(xys.keys(), xys.values(), 'Turno', 'Reportes')
         plot.show(col)
         print()
         print()
         print()
 
+def instances_dictionary(instances, dates):
+    # Make an dictionary of dates and instances (zeros included)
+    partial_instances = dict() # instances present in the data
+    for k, v in instances.iteritems():
+        partial_instances[k.date()] = v
 
+    instances = dict() # instances considering all day in a year
+    for day in dates:
+        instances[day] = 0 if day not in partial_instances.keys() else partial_instances[day]
+    
+    return instances
+
+def daily_reports(df, plot_length):
+    # BUILD ALL DATES
+    start_date = df['FECHA_REAL'].min().date()
+    end_date = df['FECHA_REAL'].max().date()
+
+    # Make date range
+    delta = end_date - start_date
+
+    dates = []
+    for i in range(delta.days + 1):
+        dates.append(start_date + timedelta(days=i))
+
+    # Get number of reports
+    instances_by_day = df['FECHA_REAL'].dt.floor('d').value_counts()
+    instances = instances_dictionary(instances_by_day, dates)
+
+    plot_length = plot_length if plot_length < 143 else 142
+
+    plot = plt(plot_length)
+    plot.set_values(instances.keys(), instances.values(), 'Día', 'Reportes')
+    plot.show('Reportes por día')
+
+def monthly_report(df, plot_length):
+    instances_by_month = df['FECHA_REAL'].groupby([df.FECHA_REAL.dt.year, df.FECHA_REAL.dt.month]).agg('count')
+    instances_by_month = pd.DataFrame(instances_by_month)
+    instances_by_month['year_month'] = instances_by_month.index.to_series().apply(lambda x: '{0}-{1:02}'.format(*x))
+
+    plot_length = plot_length if plot_length < 143 else 142
+
+    plot = plt(plot_length)
+    plot.set_values(instances_by_month['year_month'], instances_by_month['FECHA_REAL'], 'Mes', 'Reportes')
+    plot.show('Reportes por mes')
+
+def repetitions_analysis(df):
+    # For each column, counts the values and prints the first 8 rows
+    for col in df.columns:
+        values = pd.DataFrame(df[col].value_counts()[:8])
+        values['percentage'] = values / 59 * 100
+        print(values)
+        print()
+
+def numeric_analysis(df, cm = None):
+    if cm is None:
+        df_int = df.select_dtypes('number')
+    else:
+        df_int = df[df['CMANTENI'] == cm].select_dtypes('number')
+    print(df_int.describe())
 
 def main():
     df = load_data()
 
     # SPLIT DATA BY COLUMNS' TYPE
-    date_cols = df.select_dtypes('datetime')
-    int_cols = df.select_dtypes('number')
-    obj_cols = df.select_dtypes(object)
+    # date_cols = df.select_dtypes('datetime')
 
     # hour analysis
-    hourly_reports(obj_cols, 'CMABS')
+    hourly_reports(df, 60, cm_to_filter = 'CMABS', col = 'HORA_REAL')
+    # hourly_reports(df, 60, col = 'HORA_REAL')
+
+    # daily reports - seem useless
+    # daily_reports(df[df['CMANTENI'] == 'CMABS'], 40)
+    # daily_reports(df, 142)
+
+    # monthly reports
+    monthly_report(df[df['CMANTENI'] == 'CMABS'], 142)
+    # monthly_report(df, 142)
+
+    df_obj = df.select_dtypes(object)
+    repetitions_analysis(df_obj[df_obj['CMANTENI'] == 'CMABS'])
+
+    numeric_analysis(df, cm = 'CMABS')
 
 if __name__ == '__main__':
     main()
