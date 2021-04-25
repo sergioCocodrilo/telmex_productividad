23521857cc81e439c499d0144ccae2505e44fb13
sergio <sergio.solano.p@gmail.com>
2021-04-24 21:52:45 -0500

    moved analysis to a class to encapsulate the analysis with OO

--- rda_v3.py
+++ rda_v3.py
@@ -25,8 +25,22 @@ from typing import Sequence
 
 from plotter import Plotter as plt
 
+
+def instances_dictionary(instances: pd.Series, dates: list):
+    # Make an dictionary of dates and instances (zeros included)
+    partial_instances = dict() # instances present in the data
+    for k, v in instances.iteritems():
+        partial_instances[k.date()] = v
+
+    instances = dict() # instances considering all day in a year
+    for day in dates:
+        instances[day] = 0 if day not in partial_instances.keys() else partial_instances[day]
+    
+    return instances
+
+
 class Productivity_Analizer:
-    def __init__(self, sheet_name, directory, file_prefix: str = 'rdat_metro'):
+    def __init__(self, sheet_name: str, directory: str, cm: str = None, file_prefix: str = 'rdat_metro'):
         '''Loads the Excel reports of the productivity and returns a single df.'''
         hostname = socket.gethostname()
         if directory is None:
@@ -44,161 +58,156 @@ class Productivity_Analizer:
                 df_tmp = pd.read_excel(directory + f, sheet_name = sheet_name)
                 columns.append(df_tmp.columns)
                 df = df.append(df_tmp)
+
+        # filter if required
+        if cm is not None:
+            df = df[df['CMANTENI'] == cm]
+            if df.shape[0] < 1:
+                print('Your CM filter resulted in no rows.')
+                raise ValueError('Your CM input produced an empty data frame.')
                 
         self.df = df
+        # plotting structure
+        self.plotting_data = []
 
+    def hourly_reports(self):
+        '''
+        Analysis of the reports by hour of ocurrence.
+        Three types of time data are considered:
+            - HORATLMI
+            - HORACTEI
+            - HORATLMF
+            - HORA_REAL
 
+        Additionally, reports are grouped by ship, i.e.:
+            - matutino
+            - vespertino
+            - nocturno
 
-##############################
-# TIME-RELATED REPORTS
-##############################
-def hourly_reports(df: pd.DataFrame) -> list[dict]:
-    '''
-    Analysis of the reports by hour of ocurrence.
-    Three types of time data are considered:
-        - HORATLMI
-        - HORACTEI
-        - HORATLMF
-        - HORA_REAL
+        Returns a list of dictionaries with the structure:
+            [
+                {
+                    'title': 'HORA_REAL',
+                    'cols': (col_a_title, col_b_title),
+                    'xys': {
+                        'x0': 'y0',
+                        'x1': 'y1',
+                        ...
+                        },
+                },
+                {
+                    'title': 'HORATLMF',
+                    'cols': (col_a_title, col_b_title),
+                    'xys': {
+                        'x0': 'y0',
+                        'x1': 'y1',
+                        ...
+                        },
+                },
+            ]
 
-    Additionally, reports are grouped by ship, i.e.:
-        - matutino
-        - vespertino
-        - nocturno
-
-    Returns a list of dictionaries with the structure:
-        [
-            {
-                'title': 'HORA_REAL',
-                'cols': (col_a_title, col_b_title),
-                'xys': {
-                    'x0': 'y0',
-                    'x1': 'y1',
-                    ...
-                    },
-            },
-            {
-                'title': 'HORATLMF',
-                'cols': (col_a_title, col_b_title),
-                'xys': {
-                    'x0': 'y0',
-                    'x1': 'y1',
-                    ...
-                    },
-            },
+        '''
+        cols = [
+            'HORATLMI',
+            'HORACTEI',
+            'HORATLMF',
+            'HORA_REAL',
         ]
 
-    '''
-    cols = [
-        'HORATLMI',
-        'HORACTEI',
-        'HORATLMF',
-        'HORA_REAL',
-    ]
+        plotting_values = []
 
-    plotting_values = []
+        for i, col in enumerate(cols):
+            # remove nan values
+            hour_data = self.df[self.df[col].notna()]
+            # extract hour as int
+            time_df = pd.DataFrame(hour_data[col].astype(str).str[:2].astype(int))
 
-    for i, col in enumerate(cols):
-        # remove nan values
-        hour_data = df[df[col].notna()]
-        # extract hour as int
-        time_df = pd.DataFrame(hour_data[col].astype(str).str[:2].astype(int))
+            xy = time_df.value_counts().sort_index()
+            xys = dict()
 
-        xy = time_df.value_counts().sort_index()
-        xys = dict()
+            for h in range(24):
+                try:
+                    xys[h] = xy[h]
+                except KeyError:
+                    xys[h] = 0
 
-        for h in range(24):
-            try:
-                xys[h] = xy[h]
-            except KeyError:
-                xys[h] = 0
+            d = dict()
+            d['title'] = col
+            d['cols'] = ('HORA', 'Reportes')
+            d['xys'] = xys
+            plotting_values.append(copy.deepcopy(d))
+
+            # ship analysis
+            xys_ship = {
+                    'mat' : 0,
+                    'vesp': 0,
+                    'noct': 0,
+                    }
+
+            for x, y  in xys.items():
+                if 7 < int(x) < 17:
+                    xys_ship['mat'] += y
+                elif 16 < int(x) < 24:
+                    xys_ship['vesp'] += y
+                else:
+                    xys_ship['noct'] += y
+
+            d['title'] = col + ' por turnos'
+            d['cols'] = ('Turno', 'Reportes')
+            d['xys'] = xys_ship
+        self.plotting_data.append(copy.deepcopy(d))
+
+    def daily_reports(self):
+        # BUILD ALL DATES
+        start_date = self.df['FECHA_REAL'].min().date()
+        end_date = self.df['FECHA_REAL'].max().date()
+
+        # Make date range
+        delta = end_date - start_date
+
+        dates = []
+        for i in range(delta.days + 1):
+            dates.append(start_date + timedelta(days=i))
+
+        # Get number of reports
+        instances_by_day = self.df['FECHA_REAL'].dt.floor('d').value_counts()
+        instances = instances_dictionary(instances_by_day, dates)
 
         d = dict()
-        d['title'] = col
-        d['cols'] = ('HORA', 'Reportes')
-        d['xys'] = xys
-        plotting_values.append(copy.deepcopy(d))
+        d['title'] = 'Reportes por día'
+        d['cols'] = ('Día', 'Reportes')
+        d['xys'] = instances
+        self.plotting_data.append(d)
 
-        # ship analysis
-        xys_ship = {
-                'mat' : 0,
-                'vesp': 0,
-                'noct': 0,
-                }
+    def monthly_report(self):
+        instances_by_month = self.df['FECHA_REAL'].groupby([self.df.FECHA_REAL.dt.year, self.df.FECHA_REAL.dt.month]).agg('count')
+        instances_by_month = pd.DataFrame(instances_by_month)
+        instances_by_month['year_month'] = instances_by_month.index.to_series().apply(lambda x: '{0}-{1:02}'.format(*x))
 
-        for x, y  in xys.items():
-            if 7 < int(x) < 17:
-                xys_ship['mat'] += y
-            elif 16 < int(x) < 24:
-                xys_ship['vesp'] += y
-            else:
-                xys_ship['noct'] += y
+        d = dict()
+        d['title'] = 'Reportes por mes'
+        d['cols'] = ('Mes', 'Reportes')
+        d['xys'] = dict(zip(instances_by_month['year_month'], instances_by_month['FECHA_REAL']))
+        self.plotting_data.append(d)
 
-        d['title'] = col + ' por turnos'
-        d['cols'] = ('Turno', 'Reportes')
-        d['xys'] = xys_ship
-        plotting_values.append(copy.deepcopy(d))
-    return plotting_values
+    def repetitions_analysis(self):
+        # For each column, counts the values and prints the first 8 rows
+        df = self.df.select_dtypes(object)
+        for col in df.columns:
+            values = pd.DataFrame(df[col].value_counts()[:8])
+            values['percentage'] = values / 59 * 100
+            print(values)
+            print()
 
-def instances_dictionary(instances: pd.Series, dates: list):
-    # Make an dictionary of dates and instances (zeros included)
-    partial_instances = dict() # instances present in the data
-    for k, v in instances.iteritems():
-        partial_instances[k.date()] = v
+    def numeric_analysis(self):
+        df_int = self.df.select_dtypes('number')
+        print(df_int.describe())
 
-    instances = dict() # instances considering all day in a year
-    for day in dates:
-        instances[day] = 0 if day not in partial_instances.keys() else partial_instances[day]
-    
-    return instances
-
-def daily_reports(df: pd.DataFrame):
-    # BUILD ALL DATES
-    start_date = df['FECHA_REAL'].min().date()
-    end_date = df['FECHA_REAL'].max().date()
-
-    # Make date range
-    delta = end_date - start_date
-
-    dates = []
-    for i in range(delta.days + 1):
-        dates.append(start_date + timedelta(days=i))
-
-    # Get number of reports
-    instances_by_day = df['FECHA_REAL'].dt.floor('d').value_counts()
-    instances = instances_dictionary(instances_by_day, dates)
-
-    d = dict()
-    d['title'] = 'Reportes por día'
-    d['cols'] = ('Día', 'Reportes')
-    d['xys'] = instances
-    return [d]
-
-def monthly_report(df: pd.DataFrame):
-    instances_by_month = df['FECHA_REAL'].groupby([df.FECHA_REAL.dt.year, df.FECHA_REAL.dt.month]).agg('count')
-    instances_by_month = pd.DataFrame(instances_by_month)
-    instances_by_month['year_month'] = instances_by_month.index.to_series().apply(lambda x: '{0}-{1:02}'.format(*x))
-
-    d = dict()
-    d['title'] = 'Reportes por mes'
-    d['cols'] = ('Mes', 'Reportes')
-    d['xys'] = dict(zip(instances_by_month['year_month'], instances_by_month['FECHA_REAL']))
-    return [d]
-
-def repetitions_analysis(df: pd.DataFrame):
-    # For each column, counts the values and prints the first 8 rows
-    for col in df.columns:
-        values = pd.DataFrame(df[col].value_counts()[:8])
-        values['percentage'] = values / 59 * 100
-        print(values)
-        print()
-
-def numeric_analysis(df: pd.DataFrame, cm: str = None):
-    if cm is None:
-        df_int = df.select_dtypes('number')
-    else:
-        df_int = df[df['CMANTENI'] == cm].select_dtypes('number')
-    print(df_int.describe())
+    def plot(self):
+        for d in self.plotting_data:
+            plot = plt(140)
+            plot.set_values(d['xys'].keys(), d['xys'].values(), d['cols'][0], d['cols'][1])
+            plot.show(d['title'])
 
 def main(argv = None):
 
@@ -213,46 +222,14 @@ def main(argv = None):
     # parser.add_argument('-th', '--hora', help='Tipo de hora', type=str, required = True)
 
     args = parser.parse_args(argv)
-    df = load_data(args.sheet, args.directory, args.prefix)
 
-    if args.cm is not None:
-        df_tmp = df[df['CMANTENI'] == args.cm]
-        if df_tmp.shape[0] < 1:
-            print('Your CM filter resulted in no rows.')
-            return 0
-        df = df_tmp
-
-    # SPLIT DATA BY COLUMNS' TYPE
-    # date_cols = df.select_dtypes('datetime')
-
-    # hour analysis
-    plt_values = hourly_reports(df)
-    # for d in plt_values:
-        # plot = plt(140)
-        # plot.set_values(d['xys'].keys(), d['xys'].values(), d['cols'][0], d['cols'][1])
-        # plot.show(d['title'])
-
-
-    # daily reports - seem useless
-    plt_values = daily_reports(df)
-    # for d in plt_values:
-        # plot = plt(140)
-        # plot.set_values(d['xys'].keys(), d['xys'].values(), d['cols'][0], d['cols'][1])
-        # plot.show(d['title'])
-
-
-    # monthly reports
-    plt_values = monthly_report(df)
-    for d in plt_values:
-        plot = plt(140)
-        plot.set_values(d['xys'].keys(), d['xys'].values(), d['cols'][0], d['cols'][1])
-        plot.show(d['title'])
-
-    df_obj = df.select_dtypes(object)
-    repetitions_analysis(df_obj)
-
-    numeric_analysis(df)
-
+    analyzer = Productivity_Analizer(args.sheet, args.directory, args.cm, args.prefix)
+    analyzer.hourly_reports()
+    analyzer.daily_reports()
+    analyzer.monthly_report()
+    analyzer.plot()
+    analyzer.repetitions_analysis()
+    analyzer.numeric_analysis()
 
 if __name__ == '__main__':
     main()
