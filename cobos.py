b1730190a602fc1343679b1a278d6b58796a82da
sergio <sergio.solano.p@gmail.com>
2021-05-01 10:25:04 -0500

    splitted the plotting part

--- cobos.py
+++ cobos.py
@@ -1,6 +1,44 @@
+'''
+Columnas:
+    datos numéricos -> generan un resumen (describe)
+        ctes_inf
+        lin_pots
+        tpo_real
+        cump_tpo
+        rein_dias
+        rein
+        rang_hrs
+    datos fecha     -> generan gráficas
+        fech_ini_a
+        fech_fin_a
+        mes_rep
+    datos texto     -> genera un resumen por tipo de dato (value_counts)
+        no_boleta
+        estado
+        div_resp
+        sub_resp
+        gcia_resp
+        edif_resp
+        sigctralr
+        cm_p
+        tipo_cns
+        clase_afec
+        tip_red
+        ctral_afec
+        eq_ctral_a
+        subred
+        cau_gen
+        cau_esp
+        elem_red
+        siglas
+'''
+
 import pandas as pd
 import os
 import argparse
+import copy
+# import matplotlib.pyplot as plt
+from graphical_plotter import single_line_plot as slp
 
 def read_data(directory: str, prefix: str, sheet_name: str = None):
     df = pd.DataFrame()
@@ -33,16 +71,38 @@ def count_cobos_by_date(df: pd.DataFrame):
             'mes_rep',
             )
 
-    count_results = {}
+    dates_results = []
 
     # different ways of counting cobos:
+    d = dict()
     for column in date_columns:
-        count_results[column + ' mensual'] = series_counter(df[column].dt.month, range(1, 13))
-        count_results[column + ' por dia'] = series_counter(df[column].dt.day, range(1, 32))
-        count_results[column + ' dia de la semana'] = series_counter(df[column].dt.dayofweek, range(7))
-        count_results[column + ' por hora'] = series_counter(df[column].dt.hour, range(25))
+        d['title'] = column + ' - cobos por mes'
+        d['cols'] = ('Mes', 'Cobos')
+        d['xys'] = series_counter(df[column].dt.month, range(1, 13))
+        dates_results.append(copy.deepcopy(d))
 
-    return count_results
+        d['title'] = column + ' - cobos por día'
+        d['cols'] = ('día', 'Cobos')
+        d['xys'] = series_counter(df[column].dt.day, range(1, 32))
+        dates_results.append(copy.deepcopy(d))
+
+        d['title'] = column + ' - cobos por día de la semana'
+        d['cols'] = ('día', 'Cobos')
+        d['xys'] = series_counter(df[column].dt.dayofweek, range(1, 7))
+        dates_results.append(copy.deepcopy(d))
+
+        d['title'] = column + ' - cobos por hora'
+        d['cols'] = ('hora', 'Cobos')
+        d['xys'] = series_counter(df[column].dt.hour, range(1, 25))
+        dates_results.append(copy.deepcopy(d))
+
+
+        # dates_results[column + ' mensual'] = 
+        # dates_results[column + ' por dia'] = series_counter(df[column].dt.day, range(1, 32))
+        # dates_results[column + ' dia de la semana'] = series_counter(df[column].dt.dayofweek, range(7))
+        # dates_results[column + ' por hora'] = series_counter(df[column].dt.hour, range(25))
+
+    return dates_results
 
 def count_cobos_by_category(df: pd.DataFrame):
     df = df.select_dtypes(object)
@@ -71,6 +131,9 @@ def main(argv = None):
 
     # date analysis
     date_results = count_cobos_by_date(df)
+    # print(type(date_results))
+    # print(date_results.keys())
+    slp(date_results)
 
     # for title, counter in count_results.items():
         # print('Count of', title)
---
+++ graphical_plotter.py
@@ -0,0 +1,43 @@
+
+import matplotlib.pyplot as plt
+
+def single_line_plot(plots):
+    for plot in plots:
+        x, y = plot['xys'].keys(), plot['xys'].values()
+        plt.plot(x, y)
+        plt.title(plot['title'])
+        plt.legend()
+        plt.show()
+
+def plt_plotter(plots):
+    '''
+    plots is a list of dictionaries, each with the structure:
+        d['theme']
+        d['title']
+        d['cols']
+        d['xys']
+    '''
+    bloi_plot = []
+    paso_plot = []
+
+    for plot in plots:
+        if plot['theme'] == 'Bloqueo interno':
+            bloi_plot.append(plot)
+        else:
+            paso_plot.append(plot)
+
+    for bloi in bloi_plot:
+        x, y = bloi['xys'].keys(), bloi['xys'].values()
+        plt.plot(x, y, label = bloi['title'])
+
+    plt.title('Bloqueo interno')
+    plt.legend()
+    plt.show()
+
+    for paso in paso_plot:
+        x, y = paso['xys'].keys(), paso['xys'].values()
+        plt.plot(x, y, label = paso['title'])
+
+    plt.title('Paso interno')
+    plt.legend()
+    plt.show()
